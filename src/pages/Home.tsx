0fbcffc8c9cefd4f5b677cbed7764905ad861411
VishnuVandith <vishnuvandith@gmail.com>
2024-10-19 20:08:13 +0530

    Css Restructuring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
-import Navbar from '../components/Navbar/Navbar';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "../assets/css/Home.css";
+import Navbar from "../components/Navbar/Navbar";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader  className='head'>
+      <IonHeader className="head">
         <IonToolbar>
           <Navbar />
         </IonToolbar>
@@ -14,7 +20,7 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
+            <IonTitle size="large">H</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Home page" />
