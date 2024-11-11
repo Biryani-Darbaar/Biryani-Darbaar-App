f32542259f0fc902006ed801fe2256fb1dc53ae0
Yalamanchili Surya Teja <suryatejaprogramming@gmail.com>
2024-11-12 00:07:08 +0530

    Firebase session timing fix

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "orders and profile pages"
+git commit -m "Firebase session timing fix"
 git push 
--- src/providers/auth/firebase.ts
+++ src/providers/auth/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
@@ -17,5 +17,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    console.log("Session persistence set to LOCAL");
+  })
+  .catch((error) => {
+    console.error("Error setting persistence:", error);
+  });
 
 export { app, analytics, auth };
