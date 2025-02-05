bedd197d3b71d9f10062798a6e53e51486ac3138
Yalamanchili Surya Teja <suryatejaprogramming@gmail.com>
2025-02-05 20:19:18 +0530

    Bug fixes for npm run build issues

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,9 @@ const config: CapacitorConfig = {
   appId: "com.biryani.app",
   appName: "foodhut",
   webDir: "dist",
-  server: {
-    url: "http://13.239.77.189:5173/",
-  },
+  // server: {
+  //   url: "http://13.239.77.189:5173/",
+  // },
   // plugins: {
   //   FirebaseMessaging: {
   //     presentationOptions: ["badge", "sound", "alert"],
